10879af46ac9576f510c57a62b156d005f294d3e
Ilmo Salomäki <salomaki.ilmo@gmail.com>
2023-05-16 11:53:28 +0300

    tehtiin loput muutokset

--- main.js
+++ main.js
@@ -46,3 +46,55 @@ var userCredentials = {
 
     return false;
   }
+
+  let first_vote_up = 21
+  let first_vote_down = 11
+  let second_vote_up = 31
+  let second_vote_down = 2
+  let third_vote_up = 3
+  let third_vote_down = 18
+
+  const first_up = document.getElementById('first_up');
+  const first_down = document.getElementById('first_down');
+  const second_up = document.getElementById('second_up');
+  const second_down = document.getElementById('second_down');
+  const third_up = document.getElementById('third_up');
+  const third_down = document.getElementById('third_down');
+
+  document.getElementById('first_up').innerHTML = first_vote_up;
+  document.getElementById('first_down').innerHTML = first_vote_down;
+  document.getElementById('second_up').innerHTML = second_vote_up;
+  document.getElementById('second_down').innerHTML = second_vote_down;
+  document.getElementById('third_up').innerHTML = third_vote_up;
+  document.getElementById('third_down').innerHTML = third_vote_down;
+
+
+  first_up.addEventListener('click', function(e) {
+    first_vote_up++;
+    document.getElementById('first_up').innerHTML = first_vote_up;
+  })
+
+  first_down.addEventListener('click', function(e) {
+    first_vote_down++;
+    document.getElementById('first_down').innerHTML = first_vote_down;
+  })
+
+  second_up.addEventListener('click', function(e) {
+    second_vote_up++;
+    document.getElementById('second_up').innerHTML = second_vote_up;
+  })
+
+  second_down.addEventListener('click', function(e) {
+    second_vote_down++;
+    document.getElementById('second_down').innerHTML = second_vote_down;
+  })
+
+  third_up.addEventListener('click', function (e) {
+    third_vote_up++;
+    document.getElementById('third_up').innerHTML = third_vote_up;
+  })
+
+  third_down.addEventListener('click', function (e) {
+    third_vote_down++;
+    document.getElementById('third_down').innerHTML = third_vote_down;
+  })
